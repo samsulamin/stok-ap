50765e1d11f52c71a24d1b07f4907de3e90d09ac
samsulamin <samsul.rizqiamin@gmail.com>
2018-05-23 17:40:00 +0700

    stok-app

--- koneksi.php
+++ koneksi.php
@@ -1,9 +1,9 @@
 <?php
 class koneksi {
    private $server = "localhost";
-   private $username = "root";
-   private $password = "";
-    private $db = "uts";
+   private $username = "id4925391_prog_web";
+   private $password = "samsulamin13";
+    private $db = "id4925391_stok_barang";
 
     function getKoneksi() {
         return new mysqli($this->server, $this->username,$this->password,$this->db);
--- main.php
+++ main.php
@@ -1,16 +1,15 @@
 <html !DOCTYPE>
 <head>
 	<title> Data Mahasiswa </title>
-	<style>
-		a{
-			text-decoration: none;
-		}
-	</style>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+	<script src="https://code.jquery.com/jquery-3.1.1.slim.min.js" integrity="sha384-A7FZj7v+d/sdmMqp/nOQwliLvUsJfDHW+k9Omg/a/EheAdgtzNs3hpfag6Ed950n" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/tether/1.4.0/js/tether.min.js" integrity="sha384-DztdAPBWPRXSA/3eYEEUWrWCy7G5KFbe8fFjk5JAIxUYHKkDx6Qin1DkWx51bBrb" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
 </head>
 <body>
 <h2>Aplikasi Data Mahasiswa</h2>
 <hr>
-<a class="one" href="input.php">Tambah Data</a>
+<a href="input.php"><button type="button" class="btn btn-success">Tambah Data</button></a>
 <br>
 <br>
 <table border="1">
